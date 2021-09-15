c1042768a720feb5e9fe58c7277d50a9c40e0bb2
iamVihanga <vihangarashansilva@gmail.com>
2021-09-15 13:46:08 +0530

    build url shortner api

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const dns = require("dns");
+const urlparser = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// DB configuration
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) console.log("MongoDB has connected successfully.");
+  }
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+
+// Create Schema & Model
+const urlSchema = new Schema({
+  url: String,
+});
+let URL = mongoose.model("Url", urlSchema);
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  let bodyUrl = req.body.url;
+
+  dns.lookup(urlparser.parse(bodyUrl).hostname, (err, address) => {
+    if (!address) {
+      res.json({ error: "invalid url" });
+    } else {
+      let url = new URL({ url: bodyUrl });
+      url.save((err, data) => {
+        if (!err) {
+          // res.json({ error: "Cannot Create" });
+          res.json({ original_url: data.url, short_url: data.id });
+        }
+      });
+    }
+  });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  let id = req.params.id;
+
+  URL.findById(id, (err, data) => {
+    if (!data) {
+      res.json({ error: "invalid url" });
+    } else {
+      res.redirect(data.url);
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
